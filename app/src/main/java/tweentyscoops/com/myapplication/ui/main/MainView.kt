bfde41c1311dd181d43e12e52806e06a640735a8
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-12-09 14:35:11 +0700

    remove func unuesed

--- app/src/main/java/tweentyscoops/com/myapplication/ui/main/MainView.kt
+++ app/src/main/java/tweentyscoops/com/myapplication/ui/main/MainView.kt
@@ -5,5 +5,4 @@ import tweentyscoops.com.myapplication.dao.UserDao
 interface MainView {
     fun onGetUserInformationSuccess(data: UserDao?)
     fun onGetUserInformationError(errorMessage: String?)
-    fun showView()
 }
